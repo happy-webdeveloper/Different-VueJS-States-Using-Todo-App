50ab099ea3d5223b2553b4aca3f1b8e8a6a81419
happy-webdeveloper <sofiachen.dev@gmail.com>
2024-02-07 16:21:16 -0600

    innitial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  display:  block;
+  margin: 20px auto;
+  height: 75px;
+}
+</style>
